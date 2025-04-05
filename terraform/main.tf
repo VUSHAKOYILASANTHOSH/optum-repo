28576dd941a3949e548960ce07459122dda2a78f
VUSHAKOYILASANTHOSH <132207227+VUSHAKOYILASANTHOSH@users.noreply.github.com>
2025-04-05 13:06:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,9 @@ data "azurerm_client_config" "current" {
 
 }
 
-module "creating-random-name" {
-   source     = "./module/random-name"
-}
+#module "creating-random-name" {
+   #source     = "./module/random-name"
+#}
 locals {
     random_name = "${modules.creating-random-name.result}"
 }
@@ -15,7 +15,7 @@ locals {
 
 module "creating-resourcegroup"{
     source = "./modules/rg"
-    Resource_group_name = "${var.tf_appname}-${var.tf_env}-${var.tf_region}-rg-${local.random_name}"
+    Resource_group_name = "${var.tf_appname}-${var.tf_env}-${var.tf_region}-rg"
     location = var.tf_location
     tags = var.tags
 }
